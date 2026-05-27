a17a9a12e1d51f56dcafa8692088f239306636eb
Nawar <nawwar.d92@gmail.com>
2026-05-27 17:55:57 +0200

    added sslMode for railway

--- internal/db/db.go
+++ internal/db/db.go
@@ -13,8 +13,13 @@ var ErrDBConnection = errors.New("database: connection failed")
 // NewDB returns a new connection or connection error
 func NewDB(cfg *config.Config) (*pgx.Conn, error) {
 
-	connStr := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", cfg.DbUser, cfg.DbPwd, cfg.DbHost, cfg.DbPort, cfg.DbName)
-
+	sslMode := "disable"
+	if cfg.Env == "production" {
+	  sslMode = "require"
+	}
+	connStr := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=%s",
+  cfg.DbUser, cfg.DbPwd, cfg.DbHost, cfg.DbPort, cfg.DbName, sslMode)
+	
 	// Parses the URI and returns ConnConfig type or an error
 	dbConfig, parsingErr := pgx.ParseURI(connStr)
 	if parsingErr != nil {
